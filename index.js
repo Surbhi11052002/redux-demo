c2ac92b4a536b78e925be42e60df86269b393961
Surbhi <94106436+Surbhi11052002@users.noreply.github.com>
2023-05-28 18:02:53 +0530

    multiple stores in same reducer

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function orderCake() {
     // for any additional information you want to send
   };
 }
-
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 function restockCake(qty = 1) {
   return {
@@ -20,9 +19,26 @@ function restockCake(qty = 1) {
     payload: qty,
   };
 }
+
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+function orderIcecream() {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: 1,
+  };
+}
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
+function restockIcecream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
 //state
 const initialState = {
   numOfCakes: 10,
+  numOfIcecreams: 20,
 };
 //reducer
 //(previousState,action) => newState
@@ -39,6 +55,16 @@ const reducer = (state = initialState, action) => {
         ...state,
         numOfCakes: state.numOfCakes + action.payload,
       };
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIcecreams: state.numOfIcecreams - 1,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIcecreams: state.numOfIcecreams + action.payload,
+      };
     default:
       return state;
   }
@@ -51,12 +77,25 @@ const unsubscribe = store.subscribe(() =>
   console.log("update state", store.getState())
 );
 
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(restockCake(2));
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restockCake(2));
+// store.dispatch(orderIcecream());
+// store.dispatch(restockIcecream(2));
 
+const actions = redux.bindActionCreators(
+  { orderCake, restockCake, orderIcecream, restockIcecream },
+  store.dispatch
+);
+
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(2);
+actions.orderIcecream();
+actions.orderIcecream();
+actions.restockIcecream(2);
 unsubscribe();
 
-console.log("Initial state", store.getState());
+//console.log("Initial state", store.getState());
 store.dispatch(orderCake()); // the update statement won't show after unsubscribing..
